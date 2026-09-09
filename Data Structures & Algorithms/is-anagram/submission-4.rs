9aa58cc47f19ebf521f167f38e4bac205ff1c58f
Aiden Sarjoo <91558938+AidenSarjoo@users.noreply.github.com>
2026-09-09 13:27:08 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap; 
+
+impl Solution {
+
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let mut counts = [0i32; 26];
+
+        for (a, b) in s.bytes().zip(t.bytes()) {
+            counts[(a - b'a') as usize] += 1;
+            counts[(b - b'a') as usize] -= 1;
+        }
+
+        counts.iter().all(|&count| count == 0)
+    }
+}
