870841f474919499e3e53b4677e46dddc1665169
Aiden Sarjoo <91558938+AidenSarjoo@users.noreply.github.com>
2026-09-13 19:26:24 -0400

    Add: decorator - submission-2

---
+++ Design Patterns/decorator/submission-2.rs
@@ -0,0 +1,65 @@
+trait Coffee {
+    fn get_cost(&self) -> f64;
+}
+
+struct SimpleCoffee;
+
+impl SimpleCoffee {
+    fn new() -> Self {
+        SimpleCoffee
+    }
+}
+
+impl Coffee for SimpleCoffee {
+    fn get_cost(&self) -> f64 {
+        1.1
+    }
+}
+
+struct MilkDecorator {
+    coffee: Box<dyn Coffee>,
+}
+
+impl MilkDecorator {
+    fn new(coffee: Box<dyn Coffee>) -> Self {
+        MilkDecorator { coffee }
+    }
+}
+
+impl Coffee for MilkDecorator {
+    fn get_cost(&self) -> f64 {
+        self.coffee.get_cost() + 0.5
+    }
+}
+
+struct SugarDecorator {
+    coffee: Box<dyn Coffee>,
+}
+
+impl SugarDecorator {
+    fn new(coffee: Box<dyn Coffee>) -> Self {
+        SugarDecorator { coffee }
+    }
+}
+
+impl Coffee for SugarDecorator {
+    fn get_cost(&self) -> f64 {
+        self.coffee.get_cost() + 0.2
+    }
+}
+
+struct CreamDecorator {
+    coffee: Box<dyn Coffee>,
+}
+
+impl CreamDecorator {
+    fn new(coffee: Box<dyn Coffee>) -> Self {
+        CreamDecorator { coffee }
+    }
+}
+
+impl Coffee for CreamDecorator {
+    fn get_cost(&self) -> f64 {
+        self.coffee.get_cost() + 0.7
+    }
+}
