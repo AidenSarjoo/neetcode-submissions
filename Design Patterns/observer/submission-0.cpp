1865d8e1ce33b209acf038989320a2b44850b2d6
Aiden Sarjoo <91558938+AidenSarjoo@users.noreply.github.com>
2026-09-14 13:08:06 -0400

    Add: observer - submission-0

---
+++ Design Patterns/observer/submission-0.cpp
@@ -0,0 +1,51 @@
+#include <set>
+
+class Observer {
+public:
+    virtual void notify(string& itemName) = 0;
+};
+
+class Customer : public Observer {
+private:
+    string name;
+    int notifications;
+
+public:
+    Customer(string& name) : name(name), notifications(0) {}
+
+    void notify(string& itemName) override {
+        notifications += 1;
+    }
+
+    int countNotifications() {
+        return notifications;
+    }
+};
+
+class OnlineStoreItem {
+private:
+    string itemName;
+    int stock;
+    set<Observer*> observers;
+
+public:
+    OnlineStoreItem(string& itemName, int stock) : itemName(itemName), stock(stock) {}
+
+    void subscribe(Observer* observer) {
+        observers.insert(observer);  
+    }
+
+    void unsubscribe(Observer* observer) {
+        observers.erase(observer); 
+    }
+
+    void updateStock(int newStock) {
+        if (stock == 0 && newStock > 0) {
+            for (Observer* pt : observers) {
+                pt->notify(itemName);
+            }
+        }
+        stock = newStock;
+
+    }
+};
